df7d4d560ed8b5be12883fb56b2dee6c6536f6ac
ESosa0 <ericasosa0@gmail.com>
2016-02-10 16:26:25 +0100

    when you click on artist, you get a list of albums

--- app.js
+++ app.js
@@ -4,30 +4,64 @@ $(function() {
     this.artists = [];
   };
 
-  Spotify.prototype.search = function (event) {
+  Spotify.prototype.searchArtists = function (event) {
     event.preventDefault();
     $('li').remove();
     var searchTerm = $('.name').val(); 
-    $.get('https://api.spotify.com/v1/search?type=artist&query=' + searchTerm, this.handleArtists);
+    $.get('https://api.spotify.com/v1/search?type=artist&query=' + searchTerm, this.handle);
     $('.name').val('').blur();
   };
 
-  Spotify.prototype.handleArtists = function(response) {
-    response.artists.items.forEach(function(artist){
-      if(artist.images.length !== 0){
-        var image = '<img src="' + artist.images[0].url + '">'
-        var nameImage = '<li>' + artist.name + image + '</li>'
+
+  Spotify.prototype.searchAlbums = function (event) {
+    event.preventDefault();
+    var artist = event.currentTarget.innerText;
+    $.get('https://api.spotify.com/v1/search?q=artist:' + artist + '&type=album', this.handle);
+  }
+
+  
+  Spotify.prototype.handle = function(response) {
+    for(var key in response) {
+      var value = response[key]; 
+      value.items.forEach(function(thing){
+        if(thing.images.length !== 0){
+          var image = '<img src="' + thing.images[0].url + '">'
+          var nameImage = '<li> <a>' + thing.name + '</a>' + image + '</li>'
         $('.artists').append(nameImage);
-      } else {
-        var name = '<li>' + artist.name + '</li>' 
-        $('.artists').append(name);
-      }
-    });
-  } 
+        } else {
+          var name = '<li>' + thing.name + '</li>' 
+          $('.artists').append(name);
+        }
+      });
+    }
+  }
+
+
+
+  // Spotify.prototype.handleArtists = function(response) {
+  //   response.artists.items.forEach(function(artist){
+  //     if(artist.images.length !== 0){
+  //       var image = '<img src="' + artist.images[0].url + '">'
+  //       var nameImage = '<li> <a>' + artist.name + '</a>' + image + '</li>'
+  //       $('.artists').append(nameImage);
+  //     } else {
+  //       var name = '<li>' + artist.name + '</li>' 
+  //       $('.artists').append(name);
+  //     }
+  //   });
+  // } 
+
+  // Spotify.prototype.handleAlbums = function(response) {
+  //   response.albums.items.forEach(function(album){
+  //     console.log(album.name)
+  //   });
+  // }
 
   var spoti = new Spotify();
 
-  $('.search').on('click', spoti.search.bind(spoti));
+  $('.search').on('click', spoti.searchArtists.bind(spoti));
+  $('.artists').on('click', 'a', spoti.searchAlbums.bind(spoti));
+
 
 });
 
