66c0f38068b2ad634aab18645766a1c311a8851c
ESosa0 <ericasosa0@gmail.com>
2016-02-10 12:54:53 +0100

    changed to object oriented format

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 $(function() {
 
-  function search (event) {
+  var Spotify = function () {
+    this.artists = [];
+  };
+
+  Spotify.prototype.search = function (event) {
     event.preventDefault();
     $('li').remove();
     var searchTerm = $('.name').val(); 
-    var request = $.get('https://api.spotify.com/v1/search?type=artist&query=' + searchTerm);
-    $('.name').val('');
+    $.get('https://api.spotify.com/v1/search?type=artist&query=' + searchTerm, this.handleArtists);
+    $('.name').val('').blur();
+  };
 
-    function handleArtists (response) {
-      response.artists.items.forEach(function(artist){
-        if(artist.images.length !== 0){
-          var image = '<img src="' + artist.images[0].url + '">'
-          var nameImage = '<li>' + artist.name + image + '</li>'
-          $('.artists').append(nameImage);
-        } else {
-          var name = '<li>' + artist.name + '</li>' 
-          $('.artists').append(name);
-        }
-      });
-    } 
+  Spotify.prototype.handleArtists = function(response) {
+    response.artists.items.forEach(function(artist){
+      if(artist.images.length !== 0){
+        var image = '<img src="' + artist.images[0].url + '">'
+        var nameImage = '<li>' + artist.name + image + '</li>'
+        $('.artists').append(nameImage);
+      } else {
+        var name = '<li>' + artist.name + '</li>' 
+        $('.artists').append(name);
+      }
+    });
+  } 
 
-    request.done(handleArtists)
+  var spoti = new Spotify();
 
-  }
-
-
-
-  $('.search').on('click', search);
+  $('.search').on('click', spoti.search.bind(spoti));
 
 });
+
+
